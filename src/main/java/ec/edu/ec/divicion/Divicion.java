d02d6aba69c821fe6a11212226927daaf46fd909
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-10-30 15:29:05 -0500

    hola

---
+++ src/main/java/ec/edu/ec/divicion/Divicion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.divicion;
+
+/**
+ *
+ * @author HP
+ */
+public class Divicion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
